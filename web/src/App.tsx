585c8adbf15aeb91c1652779b8cb27bb2364947b
Thais Megumi <tha.megumi@gmail.com>
2020-08-10 21:29:26 -0300

    feat(auth): create auth context

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
-import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
 import GlobalStyle from './styles/global';
+
+import AuthContext from './context/AuthContext';
 
 const App: React.FC = () => (
   <>
-    <SignUp />
+    <AuthContext.Provider value={{ name: 'Thais' }}>
+      <SignIn />
+    </AuthContext.Provider>
     <GlobalStyle />
   </>
 );
