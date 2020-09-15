81b0a1b4d1bba25b76737c86f0d102c2c70eeca2
Thais Megumi <tha.megumi@gmail.com>
2020-09-14 21:32:22 -0300

    feat(Form): connect forms to unform package

--- web/src/hooks/auth.tsx
+++ web/src/hooks/auth.tsx
@@ -63,7 +63,7 @@ function useAuth(): AuthContextData {
   const context = useContext(AuthContext);
 
   if (!context) {
-    throw new Error('useAuth must be used within aa AuthProvider');
+    throw new Error('useAuth must be used within an AuthProvider');
   }
 
   return context;
